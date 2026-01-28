10f972a647c0071873b1b25f8265db8e2b3d68a9
Mikel Romero <mika_romeros51@unicah.edu>
2026-01-28 14:22:51 -0600

    Hola

---
+++ app/Models/cursos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class cursos extends Model
+{
+
+    protected $fillable = ['nombre_curso', 'codigo', 'detalles'];
+}
---
+++ database/migrations/2026_01_27_201623_salones.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Salones', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('capacidad');
+            $table->string('edificio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
